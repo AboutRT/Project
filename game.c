29582594ff8746221697cad1cb79a51655123e80
AboutRT <46733776+AboutRT@users.noreply.github.com>
2021-01-30 23:04:10 +0800

    扫雷
    这是一个用C语言写成的扫雷小游戏。

--- game.c
+++ game.c
@@ -1,127 +1,90 @@
-#define _CRT_SECURE_NO_WARNINGS 1
 #include "game.h"
 
-//初始化棋盘实现
-void InitBoard(char board[ROW][COL], int row, int col) {
+void InitBoard(char board[ROWS][COLS], int row, int col, char set)
+{
 	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			board[i][j] = ' ';
+		for (int j = 0; j < row; j++) {
+			board[i][j] = set;
 		}
 	}
 }
 
-//打印棋盘实现
-void DisplayBoard_0(char board[ROW][COL], int row, int col) {
-	for (int i = 0; i < row; i++) {
-			printf(" %c | %c | %c \n", board[i][0], board[i][1], board[i][2]);
-			if(i<row-1) 
-				printf("---|---|---\n");
-	}
-}
+void DisplayBoard(char board[ROWS][COLS], int row, int col)
+{
 
-void DisplayBoard_1(char board[ROW][COL], int row, int col) {
-	//打印数据
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			printf(" %c ", board[i][j]);
-			if (j < col - 1) {
-				printf("|");
-			}
+	//打印列号
+	for (int i = 0; i <= col; i++)
+	{
+		printf("%d ", i);
+	}
+	printf("\n");
+	for (int i = 1; i <= row; i++)
+	{
+		printf("%d ", i);
+		for (int j = 1; j <= col; j++)
+		{
+				printf("%c ", board[i][j]);
 		}
 		printf("\n");
-		//打印份隔行
-		if (i < row - 1) 
-		{
-			for (int j = 0; j < col; j++) {
-				printf("---");
-				if (j < col - 1) {
-					printf("|");
-				}
-			}
-			printf("\n");
-		}
 	}
 }
-//玩家下棋
-void PlayerMove(char board[ROW][COL], int row, int col)
+
+void SetMine(char board[ROWS][COLS], int row, int col)
+{
+	int count = EASY_COUNT;
+	while (count)
+	{
+		int x = rand() % row + 1;
+		int y = rand() % col + 1;
+		if (board[x][y] == '0') {
+			board[x][y] = '1';
+			count--;
+		}
+		
+	}
+}
+
+
+int get_mine_count(char mine[ROWS][COLS], int x, int  y)
+{
+	return mine[x - 1][y] +
+		mine[x - 1][y - 1] +
+		mine[x][y - 1] +
+		mine[x + 1][y - 1] +
+		mine[x + 1][y] +
+		mine[x + 1][y + 1] +
+		mine[x][y + 1]+
+		mine[x - 1][y + 1] - 8 * '0';
+}
+void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col)
 {
 	int x = 0;
 	int y = 0;
-	printf("玩家走：>\n");
-	while(1){
-		printf("请输入要下的坐标：>");
+	int win = 0;
+	while (win<row*col-EASY_COUNT)
+	{
+		printf("请输入排查雷的坐标：>");
 		scanf("%d%d", &x, &y);
-		if (x >= 1 && x <= row && y >= 1 && y <= col) {
-			if (board[x - 1][y - 1] == ' ') {
-				board[x - 1][y - 1] ='*';
+		if (x >= 1 && x <= row && y >= 1 && col <= col)
+		{
+			//坐标合法
+			//是雷
+			if (mine[x][y] == '1') {
+				printf("很遗憾，你被炸死了\n");
+				DisplayBoard(mine, row, col);
 				break;
-			}else {
-				printf("该坐标被占用!\n");
+			}
+			else {
+				//经计算周围的雷
+				int count = get_mine_count(mine, x, y);
+				show[x][y] = count + '0';
+				DisplayBoard(show, row, col);
+				win++;
 			}
 		}
-		else {
-			printf("坐标非法，请重新输入\n");
-		}
 	}
-}
-
-
-
-//电脑下棋
-void ComputerMove(char board[ROW][COL], int row, int col) {
-	int x = 0;
-	int y = 0;
-	printf("电脑走：>\n");
-	while(1){
-		x = rand() % row; //生成随机坐标
-		y = rand() % col;
-		if (board[x - 1][y - 1] == ' ') {
-			board[x - 1][y - 1] = '#';
-			break;
-		}
-	}	
-}
-
-
-int Isfull(char board[ROW][COL], int row, int col) {
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			if (board[i][j] == ' ')
-				return 0;
-		}
-	}
-	return 1;
-}
-//判别输赢
-//玩家赢
-//电脑赢
-//平局
-//继续
-char IsWin(char board[ROW][COL], int row, int col) {
-	//检查行
-	for (int i = 0; i < row; i++) {
-		if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
-			return board[i][0];
-		}
-	}
-	//检查列
-	for (int j = 0; j < col; j++) {
-		if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != ' ') {
-			return board[0][j];
-		}
-	}
-	//对角线
-	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
-		return board[0][0];
-	}
-	if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
-		return board[0][2];
-	}
-	//判断平局
-	if (1 == Isfull(board, ROW, COL)) {
-		return 'Q';
-	}
-	else {
-		return 'C';
+	if (win == row * col - EASY_COUNT) {
+		printf("恭喜你，排雷成功\n");
+		DisplayBoard(mine, row, col);
 	}
 }
--- game.h
+++ game.h
@@ -1,21 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#define ROW 3
-#define COL 3
+#define ROW 9
+#define COL 9
+#define ROWS ROW+2
+#define COLS COL+2
+#define EASY_COUNT 10
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h> //rand头文件
-//初始化棋盘声明
-void InitBoard(char board[ROW][COL], int row, int col);
+#include<time.h>
+#include<stdlib.h>
 
-//打印棋盘声明
-void DisplayBoard_0(char board[ROW][COL], int row, int col);
-void DisplayBoard_1(char board[ROW][COL], int row, int col);
-
-//玩家下棋
-void PlayerMove(char board[ROW][COL], int row, int col);
-void ComputerMove(char board[ROW][COL], int row, int col);
-
-//判别输赢
-char IsWin(char board[ROW][COL], int row, int col);
-
-int Isfull(char board[ROW][COL], int row, int col);
+void InitBoard(char board[ROWS][COLS], int row, int col,char set);
+void DisplayBoard(char board[ROWS][COLS], int row, int col);
+void SetMine(char board[ROWS][COLS], int row, int col);
+void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
